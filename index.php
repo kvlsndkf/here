9c173058d2d2fb1476dc5a139576525ceb33e5d4
Emillie <emillienicole.duarte@gmail.com>
2022-02-23 01:19:57 -0300

    navbar finalizada

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Here é uma plataforma institucional com o foco de criar um canal que facilite a comunicação entre professores e seus alunos.">
+    <link rel="stylesheet" href="./views/styles/style.css">
+    <link rel="stylesheet" href="./views/styles/index.style.css">
+    <link rel="shortcut icon" href="./views/images/favicon-16x16.png" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Berkshire+Swash&family=Manrope:wght@200;300;400;500;600;700;800&family=Sulphur+Point:wght@300&display=swap" rel="stylesheet">
+    <script type="text/javascript" src="./views/scripts/index.scripts.js"></script>
+    <title>HERE!</title>
+</head>
+<body>
+    <header>
+        <div class="box">
+            <img src="./views/images/logo.svg" class="logo" alt="Logo HERE!">
+            <p class="logo-name">Here!</p>
+        </div>
+        <ul>
+            <li><a href="#"></a>Recursos</li>
+            <li><a href="#"></a>Comunidade</li>
+        </ul>
+    </header>
+</body>
+</html>
