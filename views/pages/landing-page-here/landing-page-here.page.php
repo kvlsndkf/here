47574a19494ac58dfef2099f19446ed8f32d7ead
Emillie <emillienicole.duarte@gmail.com>
2022-03-02 16:50:06 -0300

    Organização das pastas do front

---
+++ views/pages/landing-page-here/landing-page-here.page.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"><meta name="description" content="Here é uma plataforma institucional com o foco de criar um canal que facilite a comunicação entre professores e seus alunos.">
+        <link rel="stylesheet" href="./views/styles/style.css">
+        <link rel="stylesheet" href="./hero.style.css">
+        <link rel="shortcut icon" href="../../images/favicon.png" type="image/x-icon">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Berkshire+Swash&family=Manrope:wght@200;300;400;500;600;700;800&family=Sulphur+Point:wght@300&display=swap" rel="stylesheet">
+        <script type="text/javascript" src="/views/js/index.scripts.js"></script>
+        <title>HERE!</title>
+    </head>
+    <body>
+        <!-- NAVBAR -->
+        <div class="container">
+            <header>
+                <div class="logo-box">
+                    <img src="../../images/logo.svg" class="logo" alt="Logo HERE!">
+                    <p class="logo-name">Here!</p>
+                </div>
+                <ul>
+                    <li><a href="#" class="a"></a>Recursos</li>
+                    <li><a href="#" class="a"></a>Comunidade</li>
+                </ul>
+            </header>
+        </div>
+            <!-- CONTEÚDO PARTE 1 -->
+            <div class="container">
+                <div class="part-1">
+                    <div class="left-1">
+                        <p class="title-1">Aprenda melhor juntos</p>
+                        <p class="text-1">Uma plataforma que facilitará a comunicação <br/> entre aluno e professor.</p>
+                        <input type="button" value="Entrar" class="btnEntrar-1">
+                    </div>
+                    <div class="right-1">
+                        <img src="../../images/img1.svg" class="image-1">
+                    </div>   
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
