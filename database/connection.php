f0b6addbc540608200674b0310f4801002b60813
Helena <hmiranda1818@gmail.com>
2022-03-06 16:32:07 -0300

    Testando novo envio na PR

--- database/connection.php
+++ database/connection.php
@@ -5,11 +5,6 @@ class Connection
     {
 
         $connection = new PDO(
-            //"mysql:host=fdb33.awardspace.net;
-            //dbname=3976116_bdduhel"
-            //,"3976116_bdduhel"
-            //,"948/73eDu88)44"
-
             "mysql:host=localhost;
             dbname=bdhere",
             "root",
