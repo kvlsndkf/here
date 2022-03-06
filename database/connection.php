5a1a2cd0b021846e980d542debaf46961409b1d9
Helena <hmiranda1818@gmail.com>
2022-03-06 15:50:53 -0300

    Envio do script de conexão com o banco

--- database/connection.php
+++ database/connection.php
@@ -1,3 +1,24 @@
 <?php
-    echo 'Testando 123...';
-?>
+class Connection
+{
+    public static function connection()
+    {
+
+        $connection = new PDO(
+            //"mysql:host=fdb33.awardspace.net;
+            //dbname=3976116_bdduhel"
+            //,"3976116_bdduhel"
+            //,"948/73eDu88)44"
+
+            "mysql:host=localhost;
+            dbname=bdhere",
+            "root",
+            ""
+        );
+
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $connection->exec("SET CHARACTER SET utf8");
+
+        return $connection;
+    }
+}
