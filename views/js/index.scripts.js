6eccbd2934302293a70bac2954a04cbf2d37046d
Helena Miranda da Silva <hmiranda1818@gmail.com>
2022-03-07 19:55:25 -0300

    Revert "Fix/hero responsive"

--- views/js/index.scripts.js
+++ views/js/index.scripts.js
@@ -1,5 +1,4 @@
 window.addEventListener("scroll", function(){
-    var nav = document.querySelector("nav");
-    nav.classList.toggle("sticky", window.scrollY > 0);
-    nav.classList.toggle("navbar-dark", window.scrollY > 0);
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0)
 });
