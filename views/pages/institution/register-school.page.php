2fc995f238126d1f6c6997209e3f397e8dd75bbc
Helena <hmiranda1818@gmail.com>
2022-03-11 10:22:52 -0300

    Correção GI

--- views/pages/institution/register-school.page.php
+++ views/pages/institution/register-school.page.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadasto ETEC | HERE!</title>
+    <title>Cadastro ETEC | HERE!</title>
 </head>
 <body>
     <form action="./controller/school-batch-resgistration.controller.php" name="school-batch-registration" method="post" enctype="multipart/form-data">
