2ec205d2b1e80994077e7951eb4955e4bdd9b57f
Paloma <palomatj.gomes5@gmail.com>
2022-03-12 23:41:03 -0300

    painel de usuarios

--- views/pages/painel-usuarios/painel-usuarios.php
+++ views/pages/painel-usuarios/painel-usuarios.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="shortcut icon" href="../../images/favicon.png" type="image/x-icon">
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     
@@ -30,7 +30,7 @@
         <div class="container">
             <div class="content">
                 <img src="../../images/logo-escrita-login.svg" class="img1">
-                <h1 class="name"> O que você é?</h1>
+                <h1 class="txt"> O que você é?</h1>
             </div>
 
             <div class="container-card">
@@ -73,7 +73,7 @@
                     <!-- Inicio do Quarto card, o do Aluno -->
                 <div class="card">
                     <div class="content">
-                        
+
                         <a href="#">
                         <img src="../../images/aluno.svg" class="img">
                         </a>
