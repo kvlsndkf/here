34c1c015b47c99e41666de43bc3d659191d33240
Helena <hmiranda1818@gmail.com>
2022-03-13 18:06:56 -0300

    Ajuste do background do login

--- views/pages/login/login.page.php
+++ views/pages/login/login.page.php
@@ -15,7 +15,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
-<body>
+<body class="back-login">
     <!-- Navbar Voltar -->
     <nav class="navbarlogin">
 
@@ -27,49 +27,46 @@
         </div>
     </nav>
 
-    <div class="back-login">
-        <div class="d-flex align-items-center  justify-content-center flex-column">
-            <div class="logo-escrita-login">
-                <img src="../../images/logo-escrita-login.svg" class="logo" alt="Logo HERE!">
-            </div>
+    <div class="d-flex align-items-center  justify-content-center flex-column">
+        <div class="logo-escrita-login">
+            <img src="../../images/logo-escrita-login.svg" class="logo" alt="Logo HERE!">
+        </div>
 
-            <p class="entrar mb-0 text-center">
-                Entrar no <span class="here-align-login">Here!</span>
-            </p>
+        <p class="entrar mb-0 text-center">
+            Entrar no <span class="here-align-login">Here!</span>
+        </p>
 
-            <!-- titulo 3-->
-            <p class="recomendamos mb-0 px-3">
-                Recomendamos usar o endereço de e-mail que você recebeu da sua instituição de ensino.
-            </p>
+        <!-- titulo 3-->
+        <p class="recomendamos mb-0 px-3">
+            Recomendamos usar o endereço de e-mail que você recebeu da sua instituição de ensino.
+        </p>
 
-            <!-- Formulário -->
-            <section class="px-3">
-                <form>
-                    <!-- Input do Email -->
-                    <div>
-                        <label>Email</label>
-                        <input type="email" class="input" placeholder="nome@institucional.mail.com" name="email" required>
-                    </div>
+        <!-- Formulário -->
+        <section class="px-3">
+            <form>
+                <!-- Input do Email -->
+                <div>
+                    <label>Email</label>
+                    <input type="email" class="input" placeholder="nome@institucional.mail.com" name="email" required>
+                </div>
 
-                    <!-- Input da Senha -->
-                    <div>
-                        <label>Senha</label>
-                        <input type="password" class="input" placeholder="**********" name="senha" required>
-                    </div>
+                <!-- Input da Senha -->
+                <div>
+                    <label>Senha</label>
+                    <input type="password" class="input" placeholder="**********" name="senha" required>
+                </div>
 
-                    <!-- button entrar -->
-                    <a href=""><button>Entrar</button></a>
+                <!-- button entrar -->
+                <a href=""><button>Entrar</button></a>
 
-                </form>
-            </section>
+            </form>
+        </section>
 
-            <!-- Fale Conosco -->
-            <div class="faleConosco d-flex justify-content-center width-100">
-                Fale conosco
-            </div>
+        <!-- Fale Conosco -->
+        <div class="faleConosco d-flex justify-content-center width-100">
+            Fale conosco
         </div>
     </div>
-
 
     <!--Js bootstrap-->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
