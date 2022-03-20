eee2c5133c242fcb857943b88f67c4370ce656e8
Arhur <arthuzito690@gmail.com>
2022-03-20 13:23:21 -0300

    alterções feitas

--- views/pages/secretary/secretary-index.page.php
+++ views/pages/secretary/secretary-index.page.php
@@ -43,7 +43,7 @@ try {
 <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container py-4">
         <!-- Foto -->
-        <a class="navbar-brand logo-box" href="./index.php">
+        <a class="navbar-brand logo-box" href="../../../index.php">
             <img src="../../images/logo.svg" class="logo" alt="Logo HERE!">
             <span class="logo-name">Here!</span>
         </a>
@@ -54,13 +54,18 @@ try {
         <!-- Menus -->
         <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
             <div class="navbar-nav animate-menu navbar-mine">
-                <a class="nav-link active font-header-hero current" href="#resources">Home</a>
-                <a class="nav-link active font-header-hero" href="../institution/school/list-school.page.php">Organização
-                <img src="../../images/Vector.svg" alt=""></a>
-
-
-                <a class="nav-link active font-header-hero" href="#">Estudantes</a>
-                <a class="nav-link active font-header-hero" href="#">Professores</a>
+                <a class="nav-link active font-header-hero current" href="./secretary-index.page.php">Home</a>
+                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle nav-link active font-header-hero" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Organização
+                                    </a>
+                                    <div class="dropdown-menu dropdown-menu-right customize-dropdown" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="#">Turmas</a>
+                                        <a class="dropdown-item" href="#">Cursos</a>
+                                        <a class="dropdown-item" href="#">Matérias</a>
+                                    </div>
+                                </li>
+                <a class="nav-link active font-header-hero" href="../secretary/list-secretary.page.php">Estudantes</a>
                 <a class="nav-link active font-header-hero" href="#">Sair</a>
             </div>
         </div>
@@ -73,6 +78,7 @@ try {
 
 <div class="container px-4 container-cards">
     <div class="row gx-5 line">
+
         <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6">
             <div class="card position-relative dad-card">
                 <div class="card-body card-body-mine cb-curso flex-column d-flex">Total de Cursos cadastrados
@@ -92,17 +98,14 @@ try {
                 </div>
             </div>
         </div>
-    </div>
-</div>
 
-<div class="container px-4 container-cards-2">
-    <div class="row gx-5 line">
+        
         <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6">
             <div class="card position-relative dad-card">
                 <div class="card-body card-body-mine cb-prof flex-column d-flex">Total de professores cadastrados
                     <br/>
                     <div id="text-2">212</div>
-                    <img src="../../images/dark-mini-logo.svg" alt="Logo HERE!" class="mini-logo-card position-absolute img-fluid">
+                    <img src="../../images/icon-magenta.svg" alt="Logo HERE!" class="mini-logo-card position-absolute img-fluid">
                 </div>
             </div>
         </div>
@@ -112,21 +115,26 @@ try {
                 <div class="card-body card-body-mine cb-aluno flex-column d-flex">Total de alunos cadastrados
                     <br/>
                 <div id="text-2">212</div>
-                    <img src="../../images/logo.svg" alt="" class="mini-logo-card position-absolute img fluid">
+                    <img src="../../images/icon-light.svg" alt="" class="mini-logo-card position-absolute img fluid">
                 </div>
             </div>
         </div>
         <div class="col-md-12 col-sm-8" id="table-background">
         <img class="img-fluid" src="../../images/table-white.svg" alt="">
         </div>
+
     </div>
 </div>
+
+
 
         <br/>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-            <script type="text/javascript" src="../../js/index.scripts.js"></script>
-<!-- Option 1: Bootstrap Bundle with Popper -->
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="../../../js/index.scripts.js"></script>
     </body>
 
 </html>
