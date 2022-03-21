8df03b586c04867657209090fd470c10212b1f90
Arhur <arthuzito690@gmail.com>
2022-03-21 16:59:16 -0300

    Nav-bar funcionando

--- views/pages/secretary/secretary-index.page.php
+++ views/pages/secretary/secretary-index.page.php
@@ -43,7 +43,7 @@ try {
 <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container py-4">
         <!-- Foto -->
-        <a class="navbar-brand logo-box" href="../../../index.php">
+        <a class="navbar-brand logo-box" href="./index.php">
             <img src="../../images/logo.svg" class="logo" alt="Logo HERE!">
             <span class="logo-name">Here!</span>
         </a>
@@ -65,6 +65,8 @@ try {
                                         <a class="dropdown-item" href="#" style="font-weight: var(--extra-bold);">Matérias</a>
                                     </div>
                                 </li>
+
+
                 <a class="nav-link active font-header-hero" href="../secretary/list-secretary.page.php">Estudantes</a>
                 <a class="nav-link active font-header-hero" href="#">Sair</a>
             </div>
@@ -78,7 +80,6 @@ try {
 
 <div class="container px-4 container-cards">
     <div class="row gx-5 line">
-
         <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6">
             <div class="card position-relative dad-card top-card">
                 <div class="card-body card-body-mine cb-curso flex-column d-flex">Total de Cursos cadastrados
@@ -90,7 +91,7 @@ try {
         </div>
 
         <div class="col col 12 col-sm-12 col-md-12 col-lg-12 col-xl-6">
-            <div class="card position-relative dad-card align-card top-card">
+            <div class="card position-relative dad-card top-card">
                 <div class="card-body card-body-mine cb-turm flex-column d-flex">Total de turmas cadastrados
                     <br/>
                     <div class="text-2">212</div>
@@ -98,9 +99,13 @@ try {
                 </div>
             </div>
         </div>
+    </div>
+</div>
 
+<div class="container px-4 container-cards-2">
+    <div class="row gx-5 line">
         <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6">
-            <div class="card position-relative dad-card-2 top-card">
+            <div class="card position-relative dad-card top-card">
                 <div class="card-body card-body-mine cb-prof flex-column d-flex">Total de professores cadastrados
                     <br/>
                     <div class="text-2">212</div>
@@ -110,7 +115,7 @@ try {
         </div>
         
         <div class="col col 12 col-sm-12 col-md-12 col-lg-12 col-xl-6">
-            <div class="card position-relative dad-card-2 align-card top-card">
+            <div class="card position-relative dad-card top-card">
                 <div class="card-body card-body-mine cb-aluno flex-column d-flex">Total de alunos cadastrados
                     <br/>
                 <div class="text-2">212</div>
@@ -121,23 +126,17 @@ try {
         <div class="col-md-12 col-sm-8" id="table-background">
         <img class="img-fluid" src="../../images/table-white.svg" alt="">
         </div>
-
-
     </div>
 </div>
-
-
-
 
         <br/>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="../../../js/index.scripts.js"></script>
-
-        
+            <script type="text/javascript" src="../../js/index.scripts.js"></script>
+<!-- Option 1: Bootstrap Bundle with Popper -->
     </body>
 
 </html>
