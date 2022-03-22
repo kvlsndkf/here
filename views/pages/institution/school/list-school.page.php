d85266398f6462f00cf43ebbfc1a848af0f0af73
Emillie <emillienicole.duarte@gmail.com>
2022-03-22 03:03:53 -0300

    Paginação estilizada

--- views/pages/institution/school/list-school.page.php
+++ views/pages/institution/school/list-school.page.php
@@ -179,37 +179,38 @@ try {
         $back_page = $page - 1;
         $next_page = $page + 1;
         ?>
-        <ul class="pagination">
+        <ul class="pagination justify-content-center pt-4">
             <?php
             if ($back_page != 0) { ?>
                 <li class="page-item ">
-                    <a class="page-link" href="list-school.page.php?page=<?php echo $back_page; ?>">Prev</a>
+                    <a class="page-link" href="list-school.page.php?page=<?php echo $back_page; ?>">Anterior</a>
                 </li>
             <?php  } else { ?>
                 <li class="page-item disabled">
-                    <a class="page-link">Prev</a>
+                    <a class="page-link">Anterior</a>
                 </li>
             <?php }   ?>
 
             <?php
             for ($i = 1; $i < $qnt_page + 1; $i++) {  ?>
                 <li class="page-item">
-                    <a class="page-link" href="list-school.page.php?page=<?php echo $i; ?>"><?php echo $i; ?></a>
+                    <a class="page-link" href="list-school.page.php?page=<?php echo $i; ?>"><?php echo '<div class="pagination-style">'. $i .'</div>' ?></a>
                 </li>
             <?php } ?>
 
             <?php
             if ($next_page <= $qnt_page) { ?>
                 <li class="page-item ">
-                    <a class="page-link" href="list-school.page.php?page=<?php echo $next_page; ?>">Next</a>
+                    <a class="page-link" href="list-school.page.php?page=<?php echo $next_page; ?>">Próximo</a>
                 </li>
             <?php  } else { ?>
                 <li class="page-item disabled">
-                    <a class="page-link">Next</a>
+                    <a class="page-link">Próximo</a>
                 </li>
             <?php }   ?>
         </ul>
     </div>
+    <br/>
     <!-- fim da tabela -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
